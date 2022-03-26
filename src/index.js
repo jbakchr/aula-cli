c8a304ac930bf27c61b9d837a9405b726d377b02
Jonas Bak Phillipson <jbakchr@gmail.com>
2022-03-26 15:01:56 +0100

    Initial puppeteer setup added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { Command } = require("commander");
+const puppeteer = require("puppeteer");
 
 const program = new Command();
 program.name("aula").description("Node CLI for Aula").version("1.0.0");
@@ -7,9 +8,17 @@ program
   .command("msg")
   .description("Aula messages from 'Beskeder'")
   .action(() => {
-    console.log("fetch aula messages");
+    fetchAulaMessages();
   });
 
 program.parse();
+
+async function fetchAulaMessages() {
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+  const page = await browser.newPage();
+  await page.goto("https://www.aula.dk/portal/#/login");
+}
 
 module.exports = program;
